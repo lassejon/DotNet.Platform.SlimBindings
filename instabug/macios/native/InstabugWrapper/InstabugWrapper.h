1d85dc6ea0ad63727308a797288772bc980a2480
lassejon <lasss@live.dk>
2024-06-12 15:36:53 +0200

    init

---
+++ instabug/macios/native/InstabugWrapper/InstabugWrapper.h
@@ -0,0 +1,18 @@
+//
+//  InstabugWrapper.h
+//  InstabugWrapper
+//
+//  Created by Lasse Jon Marqaurdt Frederiksen on 12/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstabugWrapper.
+FOUNDATION_EXPORT double InstabugWrapperVersionNumber;
+
+//! Project version string for InstabugWrapper.
+FOUNDATION_EXPORT const unsigned char InstabugWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstabugWrapper/PublicHeader.h>
+
+
---
+++ instabug/macios/native/InstabugWrapper/InstabugWrapperSdk.swift
@@ -0,0 +1,65 @@
+//
+//  InstabugWrapperSdk.swift
+//  InstabugWrapper
+//
+//  Created by Lasse Jon Marqaurdt Frederiksen on 12/06/2024.
+//
+
+import Foundation
+import Instabug
+
+//struct EventArgs {
+//    
+//}
+
+
+
+public class InstabugSdk {
+//    typealias EventHandler = (AnyObject, EventArgs) -> Void
+    public var WillInvokeHandler: (() -> Void)?;
+    public var DidDismissHandler: (() -> Void)?;
+    
+    public var trackUserSteps: Bool = true {
+        didSet {
+            Instabug.trackUserSteps = trackUserSteps;
+        }
+    }
+    
+    public var shouldCaptureViewHierarchy: Bool = true {
+        didSet {
+            
+        }
+    }
+    
+    public var reproStepsMode: IBGUserStepsMode = IBGUserStepsMode.disable {
+        didSet {
+//            Instabug.setReproStepsFor(<#T##issueType: IBGIssueType##IBGIssueType#>, with: reproStepsMode);
+        }
+    }
+    
+    public func startWithToken(token: String, invocationEvents: IBGInvocationEvent) {
+        Instabug.start(withToken: token, invocationEvents: invocationEvents);
+    }
+    
+    public func SetLocale(locale: IBGLocale) {
+        Instabug.setLocale(locale);
+    }
+}
+
+public class IBGCrashReportingSdk {
+    public var enabled: Bool = false {
+        didSet {
+            
+        }
+    }
+}
+
+public class IBGBugReportingSdk {
+    public var floatingButtonEdge: CoreGraphics.CGRectEdge = CoreGraphics.CGRectEdge.maxXEdge {
+        didSet {
+            
+        }
+    }
+    
+    public var floatingButtonTopOffset: Int = 40;
+}
